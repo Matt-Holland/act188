d88f37b39ab3d12af8010d4710a53a68e244b5c5
Matt Holland <matt@mattholland.com>
2016-08-08 22:44:52 -0400

    included all the fields this time

--- database/migrations/2016_08_09_014852_create_complete_data_reports_table.php
+++ database/migrations/2016_08_09_014852_create_complete_data_reports_table.php
@@ -12,16 +12,42 @@ class CreateCompleteDataReportsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('complete_data_reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('DisclosureID');
+            $table->string('ChemicalName');
+            $table->string('ChemicalFunction');
+            $table->string('Component');
+            $table->string('Concentration');
+            $table->string('Brick');
+            $table->string('TradeSecretDesc');
+            $table->string('SubmittedBy');
+            $table->string('DateTime');
+            $table->string('ManufacturerID');
+            $table->string('ManufacturerName');
+            $table->string('PhysicalAddressofManufacturer');
+            $table->string('ManufacturerContactPerson');
+            $table->string('ManufacturerContactPersonAddress');
+            $table->string('ManufacturerContactEmailAddress');
+            $table->string('ManufacturerContactPhoneNumber');
+            $table->string('TradeAssociationID');
+            $table->string('TradeAssociationName');
+            $table->string('PhysicalAddressofTradeAssociation');
+            $table->string('TradeAssociationContactPerson');
+            $table->string('TradeAssociationContactPersonAddress');
+            $table->string('TradeAssociationEmailAddress');
+            $table->string('TradeAssociationPhoneNumber');
+            $table->timestamps();
+        });
     }
 
-    /**
+    /***
      * Reverse the migrations.
      *
      * @return void
      */
     public function down()
     {
-        //
+        Schema::drop('complete_data_reports');
     }
 }
