f6e3eab9a87acd7575c098276049904c90dc2fde
Matt Holland <matt@mattholland.com>
2016-08-08 22:14:34 -0400

    changed name of table creation method

--- database/migrations/2016_08_09_012743_create_product_model_brand_names_table.php
+++ database/migrations/2016_08_09_012743_create_product_model_brand_names_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProduct_model_brand_namesTable extends Migration
+class CreateProductModelBrandNamesTable extends Migration
 {
     /**
      * Run the migrations.
