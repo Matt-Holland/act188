bbc2dddcc2155cba01505b30a5984ab2f16d2514
Keenan <maedayx@gmail.com>
2016-12-15 21:20:11 -0500

    add down migration

--- database/migrations/2016_12_12_01_recreate_products_table.php
+++ database/migrations/2016_12_12_01_recreate_products_table.php
@@ -34,7 +34,7 @@ class RecreateProductsTable extends Migration
         }
 
         Schema::table('disclosures', function (Blueprint $table) {
-            $table->foreign('product_id')->references('id')->on('products');
+           $table->foreign('product_id')->references('id')->on('products');
            $table->dropColumn('manufacturer');
            $table->dropColumn('brand');
            $table->dropColumn('model');
@@ -43,6 +43,33 @@ class RecreateProductsTable extends Migration
 
     public function down()
     {
+        Schema::table('disclosures', function (Blueprint $table) {
+            $table->string('manufacturer');
+            $table->string('brand');
+            $table->string('model');
+        });
+
+        $products = DB::select('select * from `products`');
+        foreach ($products as $product) {
+            DB::update('update disclosures set manufacturer = ? where product_id = ?', [
+                $product->manufacturer,
+                $product->id
+            ]);
+            DB::update('update disclosures set brand = ? where product_id = ?', [
+                $product->brand,
+                $product->id
+            ]);
+            DB::update('update disclosures set model = ? where product_id = ?', [
+                $product->model,
+                $product->id
+            ]);
+        }
+
+        Schema::table('disclosures', function (Blueprint $table) {
+            $table->dropForeign(['product_id']);
+            $table->dropColumn('product_id');
+        });
+
         Schema::drop('products');
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
@@ -52,12 +79,6 @@ class RecreateProductsTable extends Migration
             $table->text('product_description');
             $table->integer('product_id');
             $table->timestamps();
-        });
-
-        Schema::table('disclosures', function (Blueprint $table) {
-            $table->string('manufacturer');
-            $table->string('brand');
-            $table->string('model');
         });
     }
 }
