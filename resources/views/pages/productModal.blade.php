c31b90da7805bc9a20959bbbf06d481ec346bb0c
Keenan <maedayx@gmail.com>
2016-12-18 16:30:45 -0500

    'styling'

--- resources/views/pages/productModal.blade.php
+++ resources/views/pages/productModal.blade.php
@@ -3,13 +3,21 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">{{ $model }}</h4>
+                <h4 class="modal-title">{{ $brand }}</h4>
             </div>
             <div class="modal-body">
-                <p><h3>{{ $brand }} - {{ $manufacturer }}</h3></p>
+                <p><span class="title" style="font-weight: bold; font-variant: small-caps">Manufacturer: </span>{{ $manufacturer }}</p>
+                <p><span class="title" style="font-weight: bold; font-variant: small-caps">Model: </span>{{ $model }}</p>
                 <ul>
                     @foreach($disclosures as $disclosure)
-                        <li>{{ $disclosure['chemical_name'] }}</li>
+                        <li>
+                            <h4>{{ $disclosure['chemical_name'] }}</h4>
+                            <p>Formaldehyde is classsified as a carcinogen by a number of authoritative sources. Inhalation of
+                                formaldehyde is associated with cancer in the respiratory tract in humans and laboratory animals. Oral
+                                exposures in animals are also carcinogenic. Formaldehyde is a skin, eye and respirtaory tract irritant and
+                                sensitizer.
+                            </p>
+                        </li>
                     @endforeach
                 </ul>
             </div>
